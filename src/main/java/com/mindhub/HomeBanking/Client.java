50960bc4b461612d544ea0edaa4b4f9e44da15d7
Mauroantss <sanhuezamauricio.a@gmail.com>
2023-09-30 20:54:40 -0300

    first commit

---
+++ src/main/java/com/mindhub/HomeBanking/Client.java
@@ -0,0 +1,4 @@
+package com.mindhub.HomeBanking;
+
+public class Client {
+}
---
+++ src/main/java/com/mindhub/HomeBanking/HomeBankingApplication.java
@@ -0,0 +1,14 @@
+package com.mindhub.HomeBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// Aca le indico que estoy trabajando con Spring boot
+@SpringBootApplication
+public class HomeBankingApplication {
+	// Metodo main, punto de entrada a la app
+	public static void main(String[] args) {
+
+		SpringApplication.run(HomeBankingApplication.class, args);
+	}
+
+}
